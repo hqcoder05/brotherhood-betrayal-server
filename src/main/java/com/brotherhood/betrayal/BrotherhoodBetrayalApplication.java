ebe0ba2782bc7fd58562d8e3344ec3a6fb0ca602
hqcoder05 <hoangquocfw@gmail.com>
2026-05-15 21:23:41 +0700

    add connection string

--- src/main/java/com/brotherhood/betrayal/BrotherhoodBetrayalApplication.java
+++ src/main/java/com/brotherhood/betrayal/BrotherhoodBetrayalApplication.java
@@ -3,10 +3,13 @@ package com.brotherhood.betrayal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.brotherhood.betrayal.config.EnvConfig;
+
 @SpringBootApplication
 public class BrotherhoodBetrayalApplication {
 
 	public static void main(String[] args) {
+		new EnvConfig();
 		SpringApplication.run(BrotherhoodBetrayalApplication.class, args);
 	}
 
---
+++ src/main/java/com/brotherhood/betrayal/config/EnvConfig.java
@@ -0,0 +1,16 @@
+package com.brotherhood.betrayal.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvConfig {
+
+    static {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
+}
