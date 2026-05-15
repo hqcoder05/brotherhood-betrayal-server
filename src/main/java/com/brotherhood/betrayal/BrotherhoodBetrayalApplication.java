4850dc4cd9419ebae3715c670f4e159762e36472
hqcoder05 <hoangquocfw@gmail.com>
2026-05-15 13:10:45 +0700

    initial commit

---
+++ src/main/java/com/brotherhood/betrayal/BrotherhoodBetrayalApplication.java
@@ -0,0 +1,13 @@
+package com.brotherhood.betrayal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrotherhoodBetrayalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrotherhoodBetrayalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brotherhood/betrayal/BrotherhoodBetrayalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brotherhood.betrayal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrotherhoodBetrayalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
